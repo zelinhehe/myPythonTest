9274c46b766a4340601a9bcd72a01b50b56e2064
武坤 <bewithyou@126.com>
2019-05-21 09:07:14 +0800

    .

--- AdvancePython-master/chapter11/multiprocessing_test.py
+++ AdvancePython-master/chapter11/multiprocessing_test.py
@@ -26,21 +26,21 @@ if __name__ == "__main__":
     # progress.join()
     # print("main progress end")
 
-    #使用线程池
+    # 使用线程池
     pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    # result = pool.apply_async(get_html, args=(3,))
-    #
-    # #等待所有任务完成
+    result = pool.apply_async(get_html, args=(3,))
+
+    # 等待所有任务完成
     # pool.close()
     # pool.join()
-    #
+
     # print(result.get())
 
-    #imap
-    # for result in pool.imap(get_html, [1,5,3]):
+    # imap
+    # for result in pool.imap(get_html, [1, 5, 3]):
     #     print("{} sleep success".format(result))
 
-    for result in pool.imap_unordered(get_html, [1,5,3]):
+    for result in pool.imap_unordered(get_html, [1, 5, 3]):
         print("{} sleep success".format(result))
 
 
