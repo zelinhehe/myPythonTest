e2864f7bf4d215e721986124fbc9d5c70999456e
武坤 <bewithyou@126.com>
2019-05-21 18:06:37 +0800

    yield from

--- AdvancePython-master/chapter12/yield_from_example.py
+++ AdvancePython-master/chapter12/yield_from_example.py
@@ -1,34 +1,35 @@
 final_result = {}
 
-# def middle(key):
-#     while True:
-#         final_result[key] = yield from sales_sum(key)
-#         print(key+"销量统计完成！！.")
-#
-# def main():
-#     data_sets = {
-#         "bobby牌面膜": [1200, 1500, 3000],
-#         "bobby牌手机": [28,55,98,108 ],
-#         "bobby牌大衣": [280,560,778,70],
-#     }
-#     for key, data_set in data_sets.items():
-#         print("start key:", key)
-#         m = middle(key)
-#         m.send(None) # 预激middle协程
-#         for value in data_set:
-#             m.send(value)   # 给协程传递每一组的值
-#         m.send(None)
-#     print("final_result:", final_result)
-#
-# if __name__ == '__main__':
-#     main()
+
+def middle(key):
+    while True:
+        final_result[key] = yield from sales_sum(key)
+        print(key + " 销量统计完成！\n")
+
+
+def main():
+    data_sets = {
+        "面膜": [1, 2, 3],
+        "手机": [4, 5, 6, 7],
+        "大衣": [8, 9, 10, 11],
+    }
+    for key, data_set in data_sets.items():
+        print("start key:", key)
+        m = middle(key)
+        m.send(None) # 预激middle协程
+        for value in data_set:
+            print("send", key, value)
+            m.send(value)   # 给协程传递每一组的值
+        m.send(None)
+    print("final_result:", final_result)
+
 
 def sales_sum(pro_name):
     total = 0
     nums = []
     while True:
         x = yield
-        print(pro_name+"销量: ", x)
+        print("recv", pro_name, x)
         if not x:
             break
         total += x
@@ -36,13 +37,15 @@ def sales_sum(pro_name):
     return total, nums
 
 if __name__ == "__main__":
-    my_gen = sales_sum("bobby牌手机")
-    my_gen.send(None)
-    my_gen.send(1200)
-    my_gen.send(1500)
-    my_gen.send(3000)
-    try:
-        my_gen.send(None)
-    except StopIteration as e:
-        result = e.value
-        print(result)
+    main()
+
+    # my_gen = sales_sum("bobby牌手机")
+    # my_gen.send(None)
+    # my_gen.send(1200)
+    # my_gen.send(1500)
+    # my_gen.send(3000)
+    # try:
+    #     my_gen.send(None)
+    # except StopIteration as e:
+    #     result = e.value
+    #     print(result)
