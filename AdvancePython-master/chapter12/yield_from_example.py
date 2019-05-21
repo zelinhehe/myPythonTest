b7c50a686e4c548beee12f1535634e9fd32bc99d
wukun <bewithyou@126.com>
2019-05-21 07:23:23 -0700

    yield from

--- AdvancePython-master/chapter12/yield_from_example.py
+++ AdvancePython-master/chapter12/yield_from_example.py
@@ -1,30 +1,41 @@
-final_result = {}
+"""
+def g1():
+    yield from gen()
 
+def main():
+    g = g1()
+    g.send(None)
 
-def middle(key):
+# 1. main:调用方 g1:委托生成器 gen:子生成器
+# 2. yield from 会在调用方与子生成器之间建立一个双向通道
+"""
+
+def middle(result, key):  # 委托生成器
     while True:
-        final_result[key] = yield from sales_sum(key)
+        result[key] = yield from sales_sum(key)
         print(key + " 销量统计完成！\n")
 
 
-def main():
+def main():  # 调用方
     data_sets = {
         "面膜": [1, 2, 3],
         "手机": [4, 5, 6, 7],
         "大衣": [8, 9, 10, 11],
     }
+    result = {}
+
     for key, data_set in data_sets.items():
         print("start key:", key)
-        m = middle(key)
+        m = middle(result, key)
         m.send(None) # 预激middle协程
         for value in data_set:
             print("send", key, value)
             m.send(value)   # 给协程传递每一组的值
         m.send(None)
-    print("final_result:", final_result)
+    print("result:", result)
 
 
-def sales_sum(pro_name):
+def sales_sum(pro_name):  # 子生成器
     total = 0
     nums = []
     while True:
@@ -35,6 +46,7 @@ def sales_sum(pro_name):
         total += x
         nums.append(x)
     return total, nums
+
 
 if __name__ == "__main__":
     main()
